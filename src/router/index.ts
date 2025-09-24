5d8cec44cc96a942271903eb0ff1ac92ccbd3660
tiagowhuber <thuber2018@udec.cl>
2025-09-24 12:13:38 -0300

    name change view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ConsultasView from '../views/ConsultasView.vue'
 import DebugView from '../views/DebugView.vue'
+import ListaFacturasView from '@/views/ListaFacturasView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'consultas',
-      component: ConsultasView
+      name: 'facturas',
+      component: ListaFacturasView
     },
     {
       path: '/debug',
